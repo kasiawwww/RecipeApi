666851ed1e5119b494b77e77ca2e6c5f0eacc140
Katarzyna Widłak <k.widlak@microsoft.wsei.edu.pl>
2019-04-01 18:40:32 +0200

    1

---
+++ RecipeApi/Controllers/RecipesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipesController: ControllerBase
+    {
+        [HttpGet]
+        public string GetRecipes()
+        {
+            return "Recipes";
+        }
+        [HttpGet("{id}")]
+        public string GetRecipes(int id)
+        {
+            return $"Recipes {id}";
+        }
+    }
+}
